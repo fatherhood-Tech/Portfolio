38bfc212dab1204621e6934cbab7c9406022651d
Ishkevin32 <ishimwekevin32@gmail.com>
2024-07-11 12:18:06 +0200

    Al-Baqarah

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <h2>Hi From App</h2>
       <p>
-        lorem ipsum dolor sit amet, consectetur adip
+        Surah Al-Baqarah
       </p>
     </div>
   );

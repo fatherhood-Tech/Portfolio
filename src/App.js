86adbc6ed7a913a5f5770d581dee6924e386cdde
sunzu chris <sunzuchris250@gmail.com>
2024-07-12 14:28:14 +0200

    to be deleted

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import OurServices from './components/OurServices';
 
 function App() {
   return (
     <div className="App">
       <h2>Hi From App</h2>
-      <p>
-        lorem ipsum dolor sit amet, consectetur adip
-      </p>
+      <OurServices />
     </div>
   );
 }
---
+++ src/components/OurServices.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './OurService.css';
+
+export default function OurService() {
+
+    return (
+        <div className="container">
+            <div className="left">
+               <span>OUR SERVICES</span>
+                 <h1>WHAT WE DO</h1>
+                 <h4>We are a global product design agency.</h4>
+                 <p>
+                     We help businesses design, develop, & ship world-class digital products.
+                 </p>
+             </div>
+             <div className="right">
+                 <h4>We are a global product design agency.</h4>
+                 {/* <img src=""></img> */}
+             </div>
+         </div>
+    )
+}
+        
